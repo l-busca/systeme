4e43fea4d112ae2981592d87c5c02243c395b783
Kyouis <45875365+Kyouis@users.noreply.github.com>
2022-01-16 19:20:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+import threading
+
 import cv2
 
-body_cascade = cv2.CascadeClassifier("./haarcascade_fullbody.xml")
-cap = cv2.VideoCapture('sample.mp4')
 
-while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    body = body_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4)
-    for x, y, w, h in body:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    if cv2.waitKey(1) == ord('q'):
-        break
-    cv2.imshow('video', frame)
-cap.release()
-cv2.destroyAllWindows()
+def recherche(fichier):
+    body_cascade = cv2.CascadeClassifier(fichier)
+    cap = cv2.VideoCapture('sample.mp4')
+
+    while True:
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        body = body_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4)
+        for x, y, w, h in body:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        if cv2.waitKey(1) == ord('q'):
+            break
+        cv2.imshow('video', frame)
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+t1 = threading.Thread(target=recherche, args=("./haarcascade_fullbody.xml",))
