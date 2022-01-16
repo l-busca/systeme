8fbe120218be412c9af5ed62232ba19a9d972078
Kyouis <45875365+Kyouis@users.noreply.github.com>
2022-01-16 19:46:32 +0100

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import threading
 
 import cv2
 
+liste = []
 
 def recherche(fichier):
     body_cascade = cv2.CascadeClassifier(fichier)
@@ -10,9 +11,10 @@ def recherche(fichier):
     while True:
         ret, frame = cap.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        body = body_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=4)
+        body = body_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=3)
         for x, y, w, h in body:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            liste.append(f'{x} {y}')
         if cv2.waitKey(1) == ord('q'):
             break
         cv2.imshow('video', frame)
@@ -20,6 +22,18 @@ def recherche(fichier):
     cv2.destroyAllWindows()
 
 
-t1 = threading.Thread(target=recherche, args=("./haarcascade_fullbody.xml",))
+def collecte():
+    while True:
+        with open('localisation.txt') as f:
+            for x in liste:
+                f.write(f'{x}\n')
 
+        print(liste)
+
+
+t1 = threading.Thread(target=recherche, args=("./haarcascade_fullbody.xml",))
+t2 = threading.Thread(target=collecte, args=())
+t1.start()
+t2.start()
 t1.join()
+t2.join()
